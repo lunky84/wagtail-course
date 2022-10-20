3f285b17b1a1f2d4f50ceb75e0a69011f4259e77
lunky84 <john.downie@thekeysupport.com>
2022-10-20 16:16:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Wagtail course
+
+### Setup codebase
+
+```shell
+cd ~/projects/personal
+git clone git@github.com:lunky84/wagtail-course.git
+cd wagtail-course
+```
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ywg2%9iusbrmpo*9*94_3md$*6ea8c&#cbb-o2@wi9x2pzn2o0"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
