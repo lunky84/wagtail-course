ee85ebf3c2a1e2bc1abfcaacda34dbfe7a155c6d
lunky84 <john.downie@thekeysupport.com>
2022-10-21 12:34:38 +0000

    Add custom homepage

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ cd ~/projects/personal
 git clone git@github.com:lunky84/wagtail-course.git
 cd wagtail-course
 ```
+
+### Run vscode dev container
+
+```shell
+devcontainer open
+```
+
+### Run the server
+```shell
+python manage.py runserver
+```
+
+View the site at http://127.0.0.1:8000/
+
+### Make migrations
+
+```shell
+python manage.py makemigrations polls
+```
+
+### Run migrations
+
+```shell
+python manage.py migrate
+```
--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 from wagtail.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
-    pass
+    """Home page model."""
+
+    templates = "templates/home/home_page.html"
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+
+    class Meta:
+
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
